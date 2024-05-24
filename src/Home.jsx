7aedfeec73c55f0f63f0e6b5ae8e15818b43f25b
jeff <diogoworkss@gmail.com>
2024-05-23 22:38:31 -0300

    feat: adicionando paginação na home e utilizando bootstrap

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,26 +4,44 @@ import 'firebase/compat/firestore';
 import './Home.css';
 import logo from './storage/img/WhatsApp_Image_2023-12-24_at_15.19.50-removebg-preview.png';
 
+const PRODUCTS_PER_PAGE = 8; // Número de produtos por página
+
 function Home() {
     const [produtos, setProdutos] = useState([]);
+    const [lastDoc, setLastDoc] = useState(null);
+    const [firstDoc, setFirstDoc] = useState(null);
+    const [hasMore, setHasMore] = useState(true);
+    const [hasPrev, setHasPrev] = useState(false);
     const [modalVisible, setModalVisible] = useState(false);
     const [selectedImage, setSelectedImage] = useState(null);
 
-    useEffect(() => {
-        const fetchProdutos = async () => {
-            try {
-                const produtosCollection = await firebase.firestore().collection('produtos').get();
-                const produtosData = produtosCollection.docs.map(doc => ({
-                    id: doc.id,
-                    ...doc.data()
-                }));
-                setProdutos(produtosData);
-            } catch (error) {
-                console.error('Erro ao buscar produtos:', error);
+    const fetchProdutos = async (direction) => {
+        try {
+            let query = firebase.firestore().collection('produtos').orderBy('name').limit(PRODUCTS_PER_PAGE);
+            if (direction === 'next' && lastDoc) {
+                query = query.startAfter(lastDoc);
+            } else if (direction === 'prev' && firstDoc) {
+                query = query.endBefore(firstDoc);
             }
-        };
 
-        fetchProdutos();
+            const snapshot = await query.get();
+            const produtosData = snapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data()
+            }));
+
+            setProdutos(produtosData);
+            setFirstDoc(snapshot.docs[0]);
+            setLastDoc(snapshot.docs[snapshot.docs.length - 1]);
+            setHasMore(snapshot.docs.length === PRODUCTS_PER_PAGE);
+            setHasPrev(direction === 'next' || produtos.length > PRODUCTS_PER_PAGE);
+        } catch (error) {
+            console.error('Erro ao buscar produtos:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchProdutos('next');
     }, []);
 
     const openModal = (imageUrl) => {
@@ -44,7 +62,7 @@ function Home() {
             </header>
 
             <div className="catalog">
-                <div className="produtos-container">
+                <div className="produtos-container ">
                     {produtos.map(produto => (
                         <div className="produto" key={produto.id}>
                             <img
@@ -57,6 +75,19 @@ function Home() {
                             <p className='valor_produto'>R$ {produto.price}</p>
                         </div>
                     ))}
+
+
+                </div>
+                <div className='d-flex justify-content-center'>
+
+                    <div className="pagination">
+                        <button style={{ backgroundColor: '#fff', color: '#000' }} onClick={() => fetchProdutos('prev')} disabled={!hasPrev}>
+                            Anterior
+                        </button>
+                        <button style={{ backgroundColor: '#fff', color: '#000', marginLeft: '3px' }} onClick={() => fetchProdutos('next')} disabled={!hasPrev}>
+                            Próximo
+                        </button>
+                    </div>
                 </div>
             </div>
 
